539f26da1b3d1f59456ac96b1e55b38caa50c652
Artem Pavlov <artem.pavlov@semrush.com>
2023-12-02 00:54:44 +0100

    added weather data

---
+++ src/weather.ts
@@ -0,0 +1,112 @@
+export type WeatherRespons = {
+    current: {
+        time: string,
+        temperature_2m: number,
+        wind_speed_10m: number,
+        rain: number,
+        showers: number,
+        snowfall: number,
+    },
+    hourly: {
+        time: string[],
+        temperature_2m: number[],
+        wind_speed_10m: number[],
+        rain: number[],
+        showers: number[],
+        snowfall: number[],
+    }
+};
+
+const normalizeDigit = (d: number) => d < 10 ? `0${d}` : String(d);
+const dateToQuery = (d: Date) => `${d.getFullYear()}-${normalizeDigit(d.getMonth() + 1)}-${normalizeDigit(d.getDate())}`;
+const getStartDate = () => dateToQuery(new Date());
+const getEndDate = () => {
+    const d = new Date();
+    d.setDate(d.getDate() + 1);
+    return dateToQuery(d);
+};
+
+export const fetchWeather = async (latitude: string, longitude: string) => {
+    const params = new URLSearchParams([
+        ['latitude', latitude],
+        ['longitude', longitude],
+        ['timezone', 'Europe/Prague'],
+        ['current', 'temperature_2m,wind_speed_10m,rain,showers,snowfall'],
+        ['hourly', 'temperature_2m,relative_humidity_2m,wind_speed_10m,rain,showers,snowfall'],
+        ['start_date', getStartDate()],
+        ['end_date', getEndDate()],
+    ]);
+
+    const response = await fetch(`https://api.open-meteo.com/v1/forecast?${params}`);
+
+    if (!response.ok) {
+        throw new Error('Unable to fetch weather data');
+    }
+
+    return response.json() as Promise<WeatherRespons>;
+}
+
+type PrecipitationType = 'rain' | 'showers' | 'snowfall';
+type Precipitation = {
+    type: PrecipitationType,
+    probability: number,
+};
+
+const findPrecipitation = (p: Record<PrecipitationType, number>): Precipitation => {
+    const [result] = Object.entries(p).sort(([, pa], [, pb]) => pb - pa);
+    return {
+        type: result[0] as PrecipitationType,
+        probability: result[1],
+    };
+};
+
+type WeatherItem = {
+    time: string,
+    temperature: number,
+    windSpeed: number,
+    precipitation: Precipitation,
+};
+type WeatherData = {
+    current: WeatherItem,
+    forecast: WeatherItem[],
+};
+
+export const parseWeatherResponse = (w: WeatherRespons): WeatherData => {
+    const result: WeatherData = {
+        current: {
+            time: w.current.time,
+            temperature: w.current.temperature_2m,
+            windSpeed: w.current.wind_speed_10m,
+            precipitation: findPrecipitation({
+                rain: w.current.rain,
+                showers: w.current.showers,
+                snowfall: w.current.snowfall,
+            }),
+        },
+        forecast: [],
+    };
+
+    const d = new Date();
+    const date = d.getDate();
+    const hour = d.getHours();
+    const startIndex = w.hourly.time.findIndex(ds => {
+        const d = new Date(ds);
+        return date === d.getDate() && hour === d.getHours();
+    });
+    const endIndex = startIndex + 6;
+
+    for (let i = startIndex; i < endIndex; i += 1) {
+        result.forecast.push({
+            time: w.hourly.time[i] ?? '',
+            temperature: w.hourly.temperature_2m[i] ?? 0,
+            windSpeed: w.hourly.wind_speed_10m[i] ?? 0,
+            precipitation: findPrecipitation({
+                rain: w.hourly.rain[i] ?? 0,
+                showers: w.hourly.showers[i] ?? 0,
+                snowfall: w.hourly.snowfall[i] ?? 0,
+            }),
+        });
+    }
+
+    return result;
+};
--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,6 +1,7 @@
 import * as assert from 'node:assert';
 import test from 'node:test';
 import { fetchDepartures, parseDepartues } from '../src/index';
+import { fetchWeather, parseWeatherResponse } from '../src/weather';
 
 test('should say hello to the world', () => {
     assert.equal('hello world', 'hello world');
@@ -10,6 +11,16 @@ test('should request data from PID API', async () => {
     const json = await fetchDepartures((process.env['STOPS'] ?? '').split(','));
 
     console.log(parseDepartues(json).join('\n'));
+
+    assert.equal(typeof json, 'object');
+});
+
+test('should fetch weather data', async () => {
+    const [lat, long] = (process.env['COORDINATES'] ?? ',').split(',');
+
+    const json = await fetchWeather(lat, long);
+
+    console.log(JSON.stringify(parseWeatherResponse(json), undefined, 2));
 
     assert.equal(typeof json, 'object');
 });
